4c601ddb007fe32b96d76fe44f03be9dfe19a4e1
Zakiya <zakiyakhansa874@gmail.com>
2026-06-03 00:19:35 +0700

    design login udah

---
+++ TugasProject PBO/Views/LoginSIMIHAN.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace TugasProject_PBO.Views
+{
+    partial class LoginSIMIHAN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginSIMIHAN));
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            linkLabel1 = new LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-338, -80);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(928, 604);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Ivory;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(linkLabel1);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(410, -1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(475, 457);
+            panel1.TabIndex = 1;
+            panel1.Paint += panel1_Paint;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(31, 113);
+            label1.Name = "label1";
+            label1.Size = new Size(77, 24);
+            label1.TabIndex = 0;
+            label1.Text = "Login to";
+            // 
+            // textBox1
+            // 
+            textBox1.ForeColor = Color.Gray;
+            textBox1.Location = new Point(32, 248);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(243, 27);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "Enter email";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(27, 143);
+            label2.Name = "label2";
+            label2.Size = new Size(267, 39);
+            label2.TabIndex = 2;
+            label2.Text = "Kesegaran Alami, ";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(27, 181);
+            label3.Name = "label3";
+            label3.Size = new Size(341, 39);
+            label3.TabIndex = 3;
+            label3.Text = "Manajemen Terintegrasi";
+            // 
+            // textBox2
+            // 
+            textBox2.ForeColor = Color.Gray;
+            textBox2.Location = new Point(32, 292);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(243, 27);
+            textBox2.TabIndex = 4;
+            textBox2.Text = "Enter password";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(22, 55);
+            label4.Name = "label4";
+            label4.Size = new Size(60, 20);
+            label4.TabIndex = 5;
+            label4.Text = "<- Back";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(29, 336);
+            label5.Name = "label5";
+            label5.Size = new Size(160, 20);
+            label5.TabIndex = 6;
+            label5.Text = "Don't have an account ";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(182, 335);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(59, 20);
+            linkLabel1.TabIndex = 7;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Sign up";
+            // 
+            // LoginSIMIHAN
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox1);
+            Name = "LoginSIMIHAN";
+            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private LinkLabel linkLabel1;
+    }
+}
---
+++ TugasProject PBO/Views/LoginSIMIHAN.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TugasProject_PBO.Views
+{
+    public partial class LoginSIMIHAN : Form
+    {
+        public LoginSIMIHAN()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
